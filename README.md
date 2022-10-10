9629c839d3664fd1be1e30059684104fe97af215
Murilo Olegini <muriloolegini@outlook.com>
2022-10-09 21:32:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">Urna eletrônica</h1>
+
+### Urna eletrônica simples para votação de condomínio usando Blockchain e Smart Contracts na rede Ethereum.
+
+---
+
+<img src="img\urna-eletronica-solidity.png">
---
+++ Votacao.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.17;
+
+contract Votacao {
+    address public sindico;
+    string public pauta;
+    
+    enum Opcao { Sim, Nao, Nulo, Abstencao }
+    
+    mapping (Opcao => address[]) voto;
+    mapping (address => bool) moradores;
+    
+    constructor (string memory _pauta){
+        sindico = msg.sender;
+        pauta = _pauta;
+    }
+    
+    function votar (Opcao _opcao) public {
+        require(!moradores[msg.sender], "Morador ja votou!");
+        voto[_opcao].push(msg.sender);
+        moradores[msg.sender] = true;
+    }
+    
+    function verResultado (Opcao _opcao) public view returns (address[] memory){
+        return (voto[_opcao]);
+    }
+}
